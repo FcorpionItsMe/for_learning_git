26dd5f15fb6524f8cec0aafb7a10de374c31066b
Firdavs <firdavsrahmonov2003@gmail.com>
2024-09-07 00:27:04 +0500

    Changing calculating of sum to calculating difarence

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func main() {
 	fmt.Scanf("%d\n", &a)
 	fmt.Println("Input B:")
 	fmt.Scanf("%d\n", &b)
-	fmt.Printf("Summ of a and b = %d", a+b);
+	fmt.Printf("Diference of a and b = %d", a-b);
 }
